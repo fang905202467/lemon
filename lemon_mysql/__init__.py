2507421d9e5b747d5d6e35177723ff9ae85fdb8b
方坤 <fangkun@gouuse.cn>
2020-05-11 17:49:43 +0800

    mysql相关内容

---
+++ lemon_mysql/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: UTF-8 -*-
+# @Time : 2020/5/11 17:34 
+# @Author : fangkun
+# @File : __init__.py.py 
+# @Software: PyCharm
---
+++ lemon_mysql/lemon_01_handle_mysql.py
@@ -0,0 +1,25 @@
+# -*- coding: UTF-8 -*-
+# @Time : 2020/5/11 17:35
+# @Author : fangkun
+# @File : lemon_01_handle_mysql.py
+# @Software: PyCharm
+
+import pymysql
+
+# 1.建立连接
+conn = pymysql.connect(host='',
+                user='',
+                password='',
+                db='',
+                port='',
+                charset='utf8',
+                cursorclass=pymysql.cursors.DictCursor
+                )
+#2.创建游标
+cursor = conn.cursor()
+
+#3.执行sql语句
+sql = "select * from "
+cursor.execute(sql)
+
+conn.commit()
